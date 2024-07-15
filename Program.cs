412233f2b65d8600d60e5219b2e08892cf087563
Matvey Korotygin <matveykaklanter@mail.ru>
2024-07-15 18:12:20 +0200

    m

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        HttpListener httpListener = new HttpListener();
+        httpListener.Prefixes.Add("http://localhost:5000/");
+        httpListener.Start();
+        Console.WriteLine("Listening...");
+
+        while (true)
+        {
+            HttpListenerContext context = await httpListener.GetContextAsync();
+            if (context.Request.IsWebSocketRequest)
+            {
+                HttpListenerWebSocketContext wsContext = await context.AcceptWebSocketAsync(null);
+                WebSocket webSocket = wsContext.WebSocket;
+
+                await Echo(webSocket);
+            }
+            else
+            {
+                context.Response.StatusCode = 400;
+                context.Response.Close();
+            }
+        }
+    }
+
+    private static async Task Echo(WebSocket webSocket)
+    {
+        var buffer = new byte[1024 * 4];
+        WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+        while (!result.CloseStatus.HasValue)
+        {
+            var receivedMessage = Encoding.UTF8.GetString(buffer, 0, result.Count);
+            Console.WriteLine("Received: " + receivedMessage);
+
+            var serverMessage = Encoding.UTF8.GetBytes("Echo: " + receivedMessage);
+            await webSocket.SendAsync(new ArraySegment<byte>(serverMessage), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+            result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+        }
+
+        await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+    }
+}
